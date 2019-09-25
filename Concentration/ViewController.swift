152e731479a17ee54d60e31ea2db692a3c0df14f
Alexander <alkaplunovskiy@gmail.com>
2019-09-25 18:38:14 +0300

    End first lecture

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
     }
-
-
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    var emojiChoices = ["👻", "🎃", "😱", "👻", "🎃", "😱"]
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardIndex = cardButtons.firstIndex(of: sender) {
+            flipCard(withEmoji: emojiChoices[cardIndex], on: sender)
+        } else {
+            print("Chosen card is not in the array")
+        }
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle(nil, for: .normal)
+            button.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
+    
 }
 
