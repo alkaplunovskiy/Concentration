b8374f98cdf66ac978a3f9550b202f8d58fad437
Alexander <alkaplunovskiy@gmail.com>
2019-09-27 23:43:30 +0300

    End 2nd lecture & implement shuffle the cards

---
+++ Concentration/Concentration.swift
@@ -0,0 +1,55 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by Alexander Kaplunovskiy on 9/27/19.
+//  Copyright © 2019 KAV. All rights reserved.
+//
+
+import Foundation
+
+class Concentration {
+    
+    var cards = [Card]()
+    
+    var indexOneAndOnlyFaceUpCard: Int?
+    
+    func chooseCard(at index: Int) {
+        // if tapped card is unmatched - do this, else - ignore this
+        if !cards[index].isMatched {
+            // if 1 card already face up, then we have indexOneAndOnlyFaceUpCard
+            if let matchIndex = indexOneAndOnlyFaceUpCard, matchIndex != index {
+                // check if cards match
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                // perform flip card at anytime and fail indexOneAndOnlyFaceUpCard, because 2 card are face up
+                cards[index].isFaceUp = true
+                indexOneAndOnlyFaceUpCard = nil
+            } else {
+                // either no cards or 2 cards are face up
+                for flipDownIndex in cards.indices {
+                    // face down all cards
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                // face up chosen card & give her index to indexOneAndOnlyFaceUpCard because 1 card face up
+                cards[index].isFaceUp = true
+                indexOneAndOnlyFaceUpCard = index
+            }
+        }
+    }
+    
+    init(numberOfPairsOfCard: Int) {
+        for _ in 1...numberOfPairsOfCard {
+            let card = Card()
+            cards += [card, card]
+        }
+        // TODO: shuffle the cards
+        for _ in 1...cards.count {
+            let randomIndex = Int(arc4random_uniform(UInt32(cards.count)))
+            let card = cards.remove(at: randomIndex)
+            cards.append(card)
+        }
+    }
+}
--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    lazy var game = Concentration(numberOfPairsOfCard: (cardButtons.count + 1) / 2)
+    
     var flipCount = 0 {
         didSet {
             flipCountLabel.text = "Flips: \(flipCount)"
@@ -19,25 +21,41 @@ class ViewController: UIViewController {
     @IBOutlet weak var flipCountLabel: UILabel!
     
     @IBOutlet var cardButtons: [UIButton]!
-    var emojiChoices = ["👻", "🎃", "😱", "👻", "🎃", "😱"]
     
     @IBAction func touchCard(_ sender: UIButton) {
         flipCount += 1
         if let cardIndex = cardButtons.firstIndex(of: sender) {
-            flipCard(withEmoji: emojiChoices[cardIndex], on: sender)
+            game.chooseCard(at: cardIndex)
+            updateViewFromModel()
         } else {
             print("Chosen card is not in the array")
         }
     }
     
-    func flipCard(withEmoji emoji: String, on button: UIButton) {
-        if button.currentTitle == emoji {
-            button.setTitle(nil, for: .normal)
-            button.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
-        } else {
-            button.setTitle(emoji, for: .normal)
-            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    func updateViewFromModel() {
+        for index in cardButtons.indices {
+            let button = cardButtons[index]
+            let card = game.cards[index]
+            if card.isFaceUp {
+                button.setTitle(emoji(for: card), for: .normal)
+                button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            } else {
+                button.setTitle(nil, for: .normal)
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 0) : #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+            }
         }
+    }
+
+    var emojiChoices = ["👻", "🎃", "😱", "💀", "🍭", "👿"]
+    
+    var emoji = [Int:String]()
+    
+    func emoji(for card: Card) -> String {
+        if emoji[card.identifier] == nil, emojiChoices.count > 0 {
+            let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
+            emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+        }
+        return emoji[card.identifier] ?? "?"
     }
     
 }
